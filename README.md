faca0b39b58ebd340ed001304e17ea03118d615c
Rob Stinogle <stinogle@gmail.com>
2014-01-07 15:42:08 -0700

    Gitignore and local config updates.

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ These files are an opinionated list of what I use when running Wordpress locally
 
 After setting up Wordpress locally, drop these files in the root directory.
 
-You'll want to rename the gitignore file and add the dot.
+You'll want to rename the wordpress.gitignore file.
 
 Then you'll need to setup your remote server information in wp-config.php and your local server info in local-config.php.
---
+++ local-config.php
@@ -0,0 +1,21 @@
+<?php
+  // ** MySQL settings - This is called only for local devlopment ** //
+
+  /** The name of the database for WordPress */
+  define('DB_NAME', 'database_name_here');
+
+  /** MySQL database username */
+  define('DB_USER', 'root');
+
+  /** MySQL database password */
+  define('DB_PASSWORD', 'root');
+
+  /** MySQL hostname */
+  define('DB_HOST', 'localhost');
+
+  /** Database Charset to use in creating database tables. */
+  define('DB_CHARSET', 'utf8');
+
+  /** The Database Collate type. Don't change this if in doubt. */
+  define('DB_COLLATE', '');
+?>
